dbaf53c2216820860792780a073d503b54f189d3
emukungu <enmukungu@gmail.com>
2018-08-22 03:57:51 +0300

    add get-all-questions route[Delivers #159866571]

---
+++ main/routes/all_questions_route.py
@@ -0,0 +1,16 @@
+from .baseRoutes import *
+
+
+@app.route('/api/v1/questions', methods = ['GET'])
+def get_all_questions():     
+    """This endpoint will fetch all questions """
+    listed_questions = []
+    if questions_list == []:
+        message = {"message":"No questions exist on this platform"}
+        return message["message"], 404
+    else:
+        for question in questions_list:
+        #return dictionary that can be jsonified easily
+            questions = question.listed_question()  
+            listed_questions.append(questions)
+        return jsonify(listed_questions), 200
---
+++ main/routes/baseRoutes.py
@@ -0,0 +1,9 @@
+from flask import request, jsonify, json
+from copy import deepcopy
+from ..models.question_model import Question
+from ..models.answer_model import Answer
+from flask_api import status
+from main import app
+
+questions_list = []
+answers_list = []
