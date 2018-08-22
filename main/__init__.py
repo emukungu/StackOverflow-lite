4d32ff5a3b5591bf909cf43a3af61a69576e872c
emukungu <enmukungu@gmail.com>
2018-08-22 04:31:10 +0300

    add post an answer route[Delivers #159866614]

--- main/__init__.py
+++ main/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from  .routes.post_a_question_route import post
+from .routes.post_a_question_route import post
 from .routes.all_questions_route import get_all_questions
 from .routes.get_specific_question import question_id
+from .routes.post_an_answer import answer
---
+++ main/routes/post_an_answer.py
@@ -0,0 +1,23 @@
+from .baseRoutes import *
+
+@app.route('/api/v1/questions/<int:questionId>/answer', methods= ['POST'])
+def answer(questionId):
+    """This endpoint will post an answer to a specific question """
+    data = request.json
+    qn_answer = data['answer']
+    user_id = data['user_id']   
+    
+    if qn_answer != "" and user_id != "":
+        for question in questions_list:
+            if question.qn_id == questionId:
+                answer = Answer(user_id, questionId, qn_answer)
+                {questionId:answer.answer_per_question()}
+                answers_list.append({questionId:answer.answer_per_question()})
+                return jsonify(answers_list)                                                         
+            else:
+                message = {"message": "No question with that id"}
+                return message["message"], 400                      
+    else:
+        message = {"message": "Please add an answer to the question"}
+        return message["message"], 400
+
