ead93e90396fe3bb2e9e8a7d13cb4a0d21ec1e12
emukungu <enmukungu@gmail.com>
2018-08-22 04:30:13 +0300

    test posting an answer[Delivers #159866614]

---
+++ test/test_post_an_answer.py
@@ -0,0 +1,14 @@
+from .test_base import *
+
+class Test_post_an_answer(TestBase):
+    def test_empty_post_answer(self):
+        self.app.post('/api/v1/questions', data = json.dumps({"title":"REST","description":"Introduction",
+                                                            "user_id":"1","date":"2018-04-03","answer":"Adopted in 20000","question_id":"1"}), content_type="application/json")
+        response = self.app.post('/api/v1/questions/1/answer', data = json.dumps({"answer":"","user_id": ""}), content_type="application/json")
+        self.assertEqual(response.status_code, 400)
+
+    def test_post_answer(self):
+        self.app.post('/api/v1/questions', data = json.dumps({"title":"REST","description":"Introduction",
+                                                            "user_id":"1","date":"2018-04-03","answer":"Adopted in 20000","question_id":"1"}), content_type="application/json")
+        response = self.app.post('/api/v1/questions/1/answer', data = json.dumps({"answer":"great", "user_id":"1"}), content_type="application/json")
+        self.assertEqual(response.status_code, 200)
