a94dfa247397514f5e07f36b796447541562a88c
emukungu <enmukungu@gmail.com>
2018-08-22 03:59:05 +0300

    test get all questions endpoint[Delivers #159866663]

---
+++ test/test_get_all_questions.py
@@ -0,0 +1,15 @@
+from .test_base import *
+
+
+class Test_get_all_questions(TestBase):
+    def test_empty_get_all_questions(self):
+        self.app.post('/api/v1/questions', data = json.dumps({"title":"","description":"",
+                                                            "user_id":"","date":"","answer":"", "qn_id":""}), content_type="application/json")
+        response = self.app.get('/api/v1/questions')
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_all_questions(self):
+        self.app.post('/api/v1/questions', data = json.dumps({"title":"REST","description":"Introduction",
+                                                            "user_id":"1","date":"2018-04-03","answer":"Adopted in 20000"}), content_type="application/json")
+        response = self.app.get('/api/v1/questions')
+        self.assertEqual(response.status_code, 200)
