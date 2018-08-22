07d59edc1b937aa56b4b63d4bd139ae20dde8f0f
emukungu <enmukungu@gmail.com>
2018-08-22 04:41:38 +0300

    test to get all questions [Delivers #159866663]

--- main/routes/baseRoutes.py
+++ main/routes/baseRoutes.py
@@ -1,5 +1,4 @@
 from flask import request, jsonify, json
-from copy import deepcopy
 from ..models.question_model import Question
 from ..models.answer_model import Answer
 from flask_api import status
--- test/test_get_all_questions.py
+++ test/test_get_all_questions.py
@@ -3,8 +3,6 @@ from .test_base import *
 
 class Test_get_all_questions(TestBase):
     def test_empty_get_all_questions(self):
-        self.app.post('/api/v1/questions', data = json.dumps({"title":"","description":"",
-                                                            "user_id":"","date":"","answer":"", "qn_id":""}), content_type="application/json")
         response = self.app.get('/api/v1/questions')
         self.assertEqual(response.status_code, 404)
 
