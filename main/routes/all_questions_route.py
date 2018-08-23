3fce73d1d2773e06ff42d3bd903c25504f265d3f
emukungu <enmukungu@gmail.com>
2018-08-24 00:11:21 +0300

    remove redundancy[Delivers #159866586, Delivers #159866571]

--- main/routes/all_questions_route.py
+++ main/routes/all_questions_route.py
@@ -1,16 +1,14 @@
-from .baseRoutes import *
+from .baseRoutes import request, jsonify, status, app, questions_list, date
 
 
 @app.route('/api/v1/questions', methods = ['GET'])
 def get_all_questions():     
     """This endpoint will fetch all questions """
     listed_questions = []
-    if questions_list == []:
-        message = {"message":"No questions exist on this platform"}
-        return message["message"], 200
-    else:
-        for question in questions_list:
+    if not questions_list:
+        return jsonify({"message":"No questions exist on this platform"}), 404
+    for question in questions_list:
         #return dictionary that can be jsonified easily
-            questions = question.listed_question()  
-            listed_questions.append(questions)
-        return jsonify(listed_questions), 200
+        questions = question.listed_question()  
+        listed_questions.append(questions)
+    return jsonify(listed_questions), 200
--- main/routes/get_specific_question.py
+++ main/routes/get_specific_question.py
@@ -1,14 +1,10 @@
-from .baseRoutes import *
+from .baseRoutes import request, jsonify, json, status, app, questions_list, date
 
 @app.route('/api/v1/questions/<int:questionId>', methods = ['GET'])
 def question_id(questionId):
     """This endpoint will fetch a specific question """
-    print(questions_list)
     for question in questions_list:
-        print(question.question_id()," ", questionId)
         if  questionId == question.question_id():
             question_details = question.question_details()        
             return jsonify(question_details), 200 
-           
-    message = {"message": "The question doesnot exist on this platform"}
-    return message["message"], 404
+    return jsonify({"message": "The question doesnot exist on this platform"}), 404
