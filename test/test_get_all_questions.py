65b1b24ce71a76b81ddcc71417cd3b0ec76d1f33
emukungu <enmukungu@gmail.com>
2018-08-24 00:12:40 +0300

    test for the response data[Delivers #159866663]

--- test/test_get_all_questions.py
+++ test/test_get_all_questions.py
@@ -1,13 +1,14 @@
-from .test_base import *
+from .test_base import TestBase, json
 
 
 class Test_get_all_questions(TestBase):
     def test_empty_get_all_questions(self):        
         response = self.app.get('/api/v1/questions')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 404)
+        self.assertIn("No questions exist on this platform", response.data.decode())
 
     def test_get_all_questions(self):
-        self.app.post('/api/v1/questions', data = json.dumps({"title":"REST","description":"Introduction",
-                                                            "user_id":"1","date":"2018-04-03","answer":"Adopted in 20000"}), content_type="application/json")
+        self.app.post('/api/v1/questions', data = self.data, content_type="application/json")
         response = self.app.get('/api/v1/questions')
         self.assertEqual(response.status_code, 200)
+        
