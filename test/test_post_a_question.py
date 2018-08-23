c7fe66584a9f3bfaa7bf363430afe2bfad702e53
emukungu <enmukungu@gmail.com>
2018-08-23 21:33:41 +0300

    test other data validation scenarios[Delivers #160000768]

--- test/test_post_a_question.py
+++ test/test_post_a_question.py
@@ -1,14 +1,23 @@
-from .test_base import *
-    
+from .test_base import TestBase, json
 class Test_post_a_question(TestBase):
 
-    def test_empty_post(self):
-        response = self.app.post('/api/v1/questions', data = json.dumps({"title":"","description":"",
-                                                            "user_id":"","date":"","answer":"", "qn_id":""}), content_type="application/json")
+    def test_missing_fields(self):
+        response = self.app.post('/api/v1/questions', data = self.empty_data, content_type="application/json")
         self.assertEqual(response.status_code, 400)
+        self.assertIn("Fill in the missing fields", response.data.decode())
 
-    def test_post_success(self):
-        response = self.app.post('/api/v1/questions', data = json.dumps({"title":"REST","description":"Introduction",
-                                                            "user_id":"1","date":"2018-04-03","answer":"Adopted in 20000"}), content_type="application/json")
+    def test_successful_post(self):
+        response = self.app.post('/api/v1/questions', data = self.data, content_type="application/json")
         self.assertEqual(response.status_code, 201)
+        self.assertIn("Your question has been posted", response.data.decode())
     
+    def test_empty_json_input(self):
+        response = self.app.post('/api/v1/questions', data = json.dumps({}), content_type="application/json")
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("Invalid inputs", response.data.decode())
+
+    def test_incorrect_values(self):
+        response = self.app.post('/api/v1/questions', data = self.incorrect_data, content_type="application/json")
+        self.assertEqual(response.status_code, 400)
+        print(bytes(response.data))
+        self.assertIn("Enter the correct values", response.data.decode())
