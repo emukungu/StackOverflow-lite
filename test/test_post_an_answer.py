a68c0fb8fa8b49fbef901c9eef86b4c4bebe3fb5
emukungu <enmukungu@gmail.com>
2018-08-24 04:12:55 +0300

    test to get paricular answers endpoint [Delivers #159866663]

--- test/test_post_an_answer.py
+++ test/test_post_an_answer.py
@@ -1,14 +1,25 @@
-from .test_base import *
+from .test_base import TestBase, json
 
 class Test_post_an_answer(TestBase):
     def test_empty_post_answer(self):
-        self.app.post('/api/v1/questions', data = json.dumps({"title":"REST","description":"Introduction",
-                                                            "user_id":"1","date":"2018-04-03","answer":"Adopted in 20000","question_id":"1"}), content_type="application/json")
+        self.app.post('/api/v1/questions', data = self.data, content_type="application/json")
         response = self.app.post('/api/v1/questions/1/answer', data = json.dumps({"answer":"","user_id": ""}), content_type="application/json")
         self.assertEqual(response.status_code, 400)
+        self.assertIn("Enter the correct values", response.data.decode())
 
     def test_post_answer(self):
-        self.app.post('/api/v1/questions', data = json.dumps({"title":"REST","description":"Introduction",
-                                                            "user_id":"1","date":"2018-04-03","answer":"Adopted in 20000","question_id":"1"}), content_type="application/json")
-        response = self.app.post('/api/v1/questions/1/answer', data = json.dumps({"answer":"great", "user_id":"1"}), content_type="application/json")
+        self.app.post('/api/v1/questions', data = self.data, content_type="application/json")
+        response = self.app.post('/api/v1/questions/1/answer', data = json.dumps({"answer":"great", "user_id":1}), content_type="application/json")
         self.assertEqual(response.status_code, 201)
+        self.assertIn("Your answer has been added", response.data.decode())
+
+    def test_empty_json_input(self):
+        response = self.app.post('/api/v1/questions/1/answer', data = json.dumps({}), content_type="application/json")
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("All data is required", response.data.decode())
+
+    def test_get_question_answers(self):
+        self.app.post('/api/v1/questions', data = self.data, content_type="application/json")
+        self.app.post('/api/v1/questions/1/answer', data = json.dumps({"answer":"great", "user_id":1}), content_type="application/json")
+        response = self.app.get('/api/v1/questions/1/answer')
+        self.assertEqual(response.status_code, 200)
