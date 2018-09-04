71377fc0204bd5ba0fecb708513788676b8403b7
emukungu <enmukungu@gmail.com>
2018-09-04 17:35:42 +0300

    add secret key to config file

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     TESTING = False
+    SECRET_KEY = 'bootcamp'
 
 class DevelopmentConfig(Config):
     DEBUG = True
