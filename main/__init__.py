4d70186d1e2f114d85ac20d693aca8b9c096afcc
emukungu <enmukungu@gmail.com>
2018-09-10 15:49:37 +0300

    add heroku extension to the app

--- main/__init__.py
+++ main/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 import psycopg2
+import connexion
+from flask.ext.heroku import Heroku
 
+
+# app = connexion.App(__name__, specification_dir = './')
 app = Flask(__name__)
 
+heroku = Heroku(app)
 
 from .db import *
 
@@ -24,6 +29,7 @@ class Create_connection:
 
 create_connection = Create_connection()
 
+# app.add_api('swagger.yaml')
 
 from .routes.post_a_question_route import post_a_question
 from .routes.all_questions_route import get_all_questions
