e0306e1c51d65d3115ababfbb6912b5668bc1d8a
emukungu <enmukungu@gmail.com>
2018-09-29 04:09:12 +0300

    host to heroku

--- main/__init__.py
+++ main/__init__.py
@@ -18,8 +18,8 @@ from .db import *
 
 class Create_connection:
     def __init__(self):
-        # if os.getenv("FLASK_ENV") == "production":
-        #     self.con = psycopg2.connect(os.getenv('DATABASE_URL?sslmode = require')) 
+        if os.getenv("FLASK_ENV") == "production":
+            self.con = psycopg2.connect(os.getenv('DATABASE_URL')) 
         self.con = psycopg2.connect(host="localhost", database="crud", user="postgres", password="postgres")
         self.cursor = self.con.cursor()
         self.cursor.execute(user)     
